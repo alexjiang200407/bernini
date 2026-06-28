81c933a82a95eb68dc49fd17ad52dfc2a436c78f
Alex Jiang <alexjiang201507@gmail.com>
2026-06-29 01:12:57 +1000

    chore: Remove legacy code

--- gfx/src/dx12/pch.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "dx12/DXError.h"
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <nvrhi/d3d12.h>
--- gfx/tests/D3D12Agility.cpp
+++
@@ -1,7 +0,0 @@
-#include <Windows.h>
-
-extern "C"
-{
-	__declspec(dllexport) extern const uint32_t D3D12SDKVersion = 618;
-	__declspec(dllexport) extern LPCSTR         D3D12SDKPath    = ".\\D3D12\\";
-}
